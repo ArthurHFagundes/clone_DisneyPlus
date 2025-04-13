8bc6dae4cf6066073a58f72ea143ba0e72132af0
ArthurHFagundes <arthur@teste.com>
2025-04-13 16:44:04 -0300

    REPOSITÓRIO || verificação de erro

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin')
 
 function styles() {
     return gulp.src('./src/styles/*.scss') // ARQUIVO FONTE
@@ -7,7 +8,13 @@ function styles() {
         .pipe(gulp.dest('./dist/css')); // ARQUIVO DE DESTINO
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/images/**/*') // ARQUIVOS FONTE
+        .pipe(imagemin()) // PARA MINIFICAR AS IMAGENS
+        .pipe(gulp.dest('./dist/images')); // PASTA DE DESTINO
+}
+
+exports.default = gulp.parallel(styles, images); // PARA EXECUTAR AS FUNÇÕES PARALELAMENTE
 
 exports.watch = function() { // EXECUTAR A FUNÇÃO DE OBSERVADOR
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles)) // 'ARQUIVO' PARA OBSERVAR E A (FUNÇÃO) PARA EXECUTAR
